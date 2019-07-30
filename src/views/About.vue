2147f163ca44e2a445bf5a020cf4249a3edbcd8d
rnsk <touch@rnsk.net>
2019-07-30 13:48:02 +0900

    Completion

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v.1.0.0. It is part of the Vue crash on Youtube</p>
+    <p><a href="https://www.youtube.com/watch?v=Wy9q22isx3U">Vue JS Crash Course - 2019</a></p>
+  </div>
+</template>
+
+<style scoped>
+  .about {
+    padding: 20px;
+  }
+</style>
